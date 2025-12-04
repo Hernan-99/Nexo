a9dd8b9967c820b4f3e8f49b74348ac286e03743
Hernan-99 <hernan24744@gmail.com>
2025-12-03 22:54:30 -0300

    first commit

---
+++ src/test/java/Nexo/app/NexoApplicationTests.java
@@ -0,0 +1,13 @@
+package Nexo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
