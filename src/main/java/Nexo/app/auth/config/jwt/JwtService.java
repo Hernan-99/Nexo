9befe50d958511ceebf8d3fcf4e2ddac5b5732d5
Hernan-99 <hernan24744@gmail.com>
2025-12-04 23:22:00 -0300

    feat: agrego servicio para manejar el jwt

---
+++ src/main/java/Nexo/app/auth/config/jwt/JwtService.java
@@ -0,0 +1,36 @@
+package Nexo.app.auth.config.jwt;
+
+import Nexo.app.auth.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 24h
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUserId(String token){
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+}
