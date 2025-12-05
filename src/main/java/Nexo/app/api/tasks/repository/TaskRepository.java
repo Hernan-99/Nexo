003628140bee4160e1d58e3244a59e64b7b24d8b
Hernan-99 <hernan24744@gmail.com>
2025-12-05 08:01:44 -0300

    Feat: agrego repository

---
+++ src/main/java/Nexo/app/api/tasks/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package Nexo.app.api.tasks.repository;
+
+import Nexo.app.api.tasks.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByUserId(UUID userId);
+}
