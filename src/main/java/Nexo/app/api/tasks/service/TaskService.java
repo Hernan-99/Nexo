6238597a4c35c7c3a4e63787dad2e47f7ac6e69a
Hernan-99 <hernan24744@gmail.com>
2025-12-05 08:21:35 -0300

    Fix: correccion en create

--- src/main/java/Nexo/app/api/tasks/service/TaskService.java
+++ src/main/java/Nexo/app/api/tasks/service/TaskService.java
@@ -1,15 +1,15 @@
 package Nexo.app.api.tasks.service;
 
+import Nexo.app.api.tasks.dto.TaskRequest;
 import Nexo.app.api.tasks.dto.TaskResponse;
 import Nexo.app.api.tasks.dto.TaskUpdateRequest;
-import Nexo.app.api.tasks.repository.TaskRepository;
 import Nexo.app.auth.model.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TaskService {
-    TaskRepository create(User user, TaskUpdateRequest req);
+    TaskResponse create(User user, TaskRequest req);
     List<TaskResponse> findAll(User user);
     TaskResponse findById(User user, UUID id);
     TaskResponse update(User user, UUID id, TaskUpdateRequest req);
