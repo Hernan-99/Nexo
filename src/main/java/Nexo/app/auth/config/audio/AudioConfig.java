858d44349650b0bdd764c20cfef2fb8eaa5982df
Hernan-99 <hernan24744@gmail.com>
2025-12-06 12:31:53 -0300

    Feat: agreo configuracion de RestTemplate para servicio de audio

---
+++ src/main/java/Nexo/app/auth/config/audio/AudioConfig.java
@@ -0,0 +1,14 @@
+package Nexo.app.auth.config.audio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AudioConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
